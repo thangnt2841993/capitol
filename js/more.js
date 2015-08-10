254b0ddbb365496ac6318bba540fb868d12ebbe3
Kenny <thangnt2841993@gmail.com>
2015-08-10 08:31:21 +0700

    version1

--- js/more.js
+++ js/more.js
@@ -47,11 +47,51 @@ $(window).scroll(function() {
 			$(this).addClass("slideDown");
 		}
 	});
+	$('#client #row-right .row').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+400) {
+			$(this).addClass("expandOpen");
+		}
+	});
+	$('#new-update #top-left').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+400) {
+			$(this).addClass("slideLeft");
+		}
+	});
+	$('#new-update #top-right').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+400) {
+			$(this).addClass("slideRight");
+		}
+	});
+	$('#new-update #bottom-left').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+400) {
+			$(this).addClass("slideDown");
+		}
+	});
+	$('#new-update #bottom-right').each(function(){
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow+400) {
+			$(this).addClass("slideDown");
+		}
+	});
 });
 $(document).ready(function(){
   	$('#map').click(function(){
     	$('#map-info').slideToggle("slow");
-    })
+    });
   	$("#carousel-owl").owlCarousel({
   		items:3
   	});
@@ -60,9 +100,13 @@ $(document).ready(function(){
   	});
   	$('#search-title').click(function(){
     	$('#search').slideToggle("slow");
-    })
+    });
     $('#scroll-top').click(function(){
         $('body,html').animate({scrollTop:0},400);
 	        return false;
-	})
+	});
+	$('#data #lines').counterUp({
+		delay: 10, // the delay time in ms
+		time: 1000, // the speed time in ms
+	});
 });
